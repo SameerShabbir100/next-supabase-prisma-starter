d09aed846d3a7397407cba7380f2f4d47daabe23
Sheikh M Sameer <112487735+SameerShabbir100@users.noreply.github.com>
2026-07-09 02:34:51 +0500

    fix(proxy): add secure cookie

--- proxy.ts
+++ proxy.ts
@@ -23,6 +23,7 @@ export async function proxy(request: NextRequest) {
   const token = await getToken({
     req: request,
     secret: process.env.NEXTAUTH_SECRET,
+    secureCookie: process.env.NODE_ENV === "production",
   });
 
   if (!token) {
